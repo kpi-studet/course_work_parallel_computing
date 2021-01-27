9b755dc8e6629cb4d869122b471a7e423956c29f
kpi-studet <letsa@ya.ru>
2021-01-27 23:10:40 +0200

    added  controller

--- src/main/java/com/kpi/parallelcomputing/KpiApplication.java
+++ src/main/java/com/kpi/parallelcomputing/KpiApplication.java
@@ -1,4 +1,4 @@
-package com.kpi.parallelcomputing.kpi;
+package com.kpi.parallelcomputing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/kpi/parallelcomputing/KpiApplicationTests.java
+++ src/test/java/com/kpi/parallelcomputing/KpiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.kpi.parallelcomputing.kpi;
+package com.kpi.parallelcomputing;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
