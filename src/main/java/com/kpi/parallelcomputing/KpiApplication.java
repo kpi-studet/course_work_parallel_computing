19d739427b7a9d8865de8f604bad6c3838f8405e
kpi-studet <letsa@ya.ru>
2021-01-27 23:40:28 +0200

    added  service

--- src/main/java/com/kpi/parallelcomputing/KpiApplication.java
+++ src/main/java/com/kpi/parallelcomputing/KpiApplication.java
@@ -1,13 +1,33 @@
 package com.kpi.parallelcomputing;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class KpiApplication {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger("course");
 
     public static void main(String[] args) {
         SpringApplication.run(KpiApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+
+            LOGGER.debug("Context has count of bean's = " + beanNames.length);
+
+        };
+    }
 }
---
+++ src/main/java/com/kpi/parallelcomputing/service/InvertIndexThreadsStarter.java
@@ -0,0 +1,8 @@
+package com.kpi.parallelcomputing.service;
+
+public class InvertIndexThreadsStarter {
+    public long startThreads (int numberOfThreads){
+        return 0;
+    }
+
+}
